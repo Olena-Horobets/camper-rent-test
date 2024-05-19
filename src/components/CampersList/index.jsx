f75227af2b4d6f9efc931a645077c41365eada7d
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2024-05-19 20:02:05 -0230

    item card stylized

---
+++ src/components/CampersList/index.jsx
@@ -0,0 +1,154 @@
+import s from './CampersList.module.css';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ReactComponent as ReactSprite } from 'images/icons.svg';
+
+import { selectCampers, selectIsLoading } from 'store/campers/selectors';
+import { getAllCampersAction } from 'store/campers/slice';
+
+import data from 'data.json';
+
+export default function CampersList() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const campers = useSelector(selectCampers);
+  const isLoading = useSelector(selectIsLoading);
+
+  useEffect(() => {
+    dispatch(getAllCampersAction());
+  }, [dispatch]);
+
+  const onOpenModalClick = id => {
+    document.querySelector('body').classList.add('bodyFixed');
+    navigate(`camper/${id}`, {
+      state: { background: location },
+    });
+  };
+
+  const normalizePrice = price =>
+    `€${price.slice(-9, -6)} ${price.slice(-6, -3)} ${price.slice(-3)},00`;
+
+  return (
+    <>
+      <ReactSprite />
+      {isLoading ? (
+        <p>...loading</p>
+      ) : (
+        <ul className={s.list}>
+          {data.map(el => {
+            return (
+              <li key={el._id} className={s.card}>
+                <img
+                  className={s.cardImg}
+                  src={el.gallery[0]}
+                  alt={el.name}
+                ></img>
+
+                <div className={s.cardContent}>
+                  <div className={s.cardHeader}>
+                    <h2 className={s.itemName}>{el.name}</h2>
+                    <span className={s.cardHeaderWrapper}>
+                      <span className={s.itemPrice}>
+                        {normalizePrice(String(el.price))}
+                      </span>
+
+                      <svg width="24" height="24">
+                        <use href="#icon-heart-empty"></use>
+                      </svg>
+                    </span>
+                  </div>
+
+                  <div className={s.cardSubHeader}>
+                    <svg width="16" height="16" className={s.itemRatingIcon}>
+                      <use href="#icon-star"></use>
+                    </svg>
+                    <span className={s.itemRating}>
+                      {el.rating} ({el.reviews?.length || 0} Reviews)
+                    </span>
+                    <svg width="16" height="16" className={s.itemLocationIcon}>
+                      <use href="#icon-location"></use>
+                    </svg>
+                    <span className={s.itemLocation}>{el.location}</span>
+                  </div>
+
+                  <p className={s.itemDesc}>{el.description}</p>
+
+                  <h3 className="visuallyHidden">Vehicle details</h3>
+                  <ul className={s.itemDetails}>
+                    <li className={s.itemDetailsPoint}>
+                      <svg width="20" height="20" className={s.itemDetailsIcon}>
+                        <use href="#icon-adults"></use>
+                      </svg>
+                      <span className={s.itemDetailsText}>
+                        {el.adults} adults
+                      </span>
+                    </li>
+                    <li className={s.itemDetailsPoint}>
+                      <svg width="20" height="20" className={s.itemDetailsIcon}>
+                        <use href="#icon-transmission"></use>
+                      </svg>
+                      <span className={s.itemDetailsTextCapital}>
+                        {el.transmission}
+                      </span>
+                    </li>
+                    <li className={s.itemDetailsPoint}>
+                      <svg width="20" height="20" className={s.itemDetailsIcon}>
+                        <use href="#icon-engine"></use>
+                      </svg>
+                      <span className={s.itemDetailsTextCapital}>
+                        {el.engine}
+                      </span>
+                    </li>
+                    {el.details.kitchen && (
+                      <li className={s.itemDetailsPoint}>
+                        <svg
+                          width="20"
+                          height="20"
+                          className={s.itemDetailsIcon}
+                        >
+                          <use href="#icon-kitchen"></use>
+                        </svg>
+                        <span className={s.itemDetailsText}>Kitchen</span>
+                      </li>
+                    )}
+                    <li className={s.itemDetailsPoint}>
+                      <svg width="20" height="20" className={s.itemDetailsIcon}>
+                        <use href="#icon-bed"></use>
+                      </svg>
+                      <span className={s.itemDetailsText}>
+                        {el.details.beds} beds
+                      </span>
+                    </li>
+                    {el.details.airConditioner && (
+                      <li className={s.itemDetailsPoint}>
+                        <svg
+                          width="20"
+                          height="20"
+                          className={s.itemDetailsIcon}
+                        >
+                          <use href="#icon-ac"></use>
+                        </svg>
+                        <span className={s.itemDetailsText}>AC</span>
+                      </li>
+                    )}
+                  </ul>
+
+                  <button
+                    onClick={() => onOpenModalClick(el._id)}
+                    className={s.cardBtn}
+                  >
+                    Show more
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
+}
--- src/pages/CampersPage/index.jsx
+++ src/pages/CampersPage/index.jsx
@@ -1,73 +1,12 @@
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { useDispatch, useSelector } from 'react-redux';
-// import { selectIsLoading } from 'store/campers/selectors';
-import { getAllCampersAction } from 'store/campers/slice';
-import { selectCampers } from 'store/campers/selectors';
+import CampersList from 'components/CampersList';
 
 function CampersPage() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  // const isLoading = useSelector(selectIsLoading);
-  const campers = useSelector(selectCampers);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getAllCampersAction());
-  }, [dispatch]);
-
-  const onOpenModalClick = id => {
-    document.querySelector('body').classList.add('bodyFixed');
-    navigate(`camper/${id}`, {
-      state: { background: location },
-    });
-  };
-
   return (
     <div>
-      <ul>
-        {campers.map(el => {
-          return (
-            <li key={el._id}>
-              <h2>{el.name}</h2>
-              <p>{el.price}</p>
-              <p>{el.rating}</p> <p>{el.location}</p>
-              {/* <p>{el.description}</p> */}
-              <h3>Vehicle details</h3>
-              <ul>
-                <li>
-                  <span>Form</span>
-                  <span>{el.form}</span>
-                </li>
-                <li>
-                  <span>Length</span>
-                  <span>{el.length}</span>
-                </li>
-                <li>
-                  <span>Width</span>
-                  <span>{el.width}</span>
-                </li>
-                <li>
-                  <span>Height</span>
-                  <span>{el.height}</span>
-                </li>
-                <li>
-                  <span>Tank</span>
-                  <span>{el.tank}</span>
-                </li>
-                <li>
-                  <span>Consumption</span>
-                  <span>{el.consumption}</span>
-                </li>
-              </ul>
-              <button onClick={() => onOpenModalClick(el._id)}>
-                Show more
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <CampersList />
+
       <Outlet></Outlet>
     </div>
   );
